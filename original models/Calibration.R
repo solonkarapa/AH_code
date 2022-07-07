c8cd43f17cfed85b5737a9a858869e9992cdc4e2
0958-AH <68644423+0958-AH@users.noreply.github.com>
2022-07-07 15:47:21 +0100

    final adjustments

--- original models/Calibration.R
+++ original models/Calibration.R
@@ -1,28 +1,33 @@
 # This script performs all calculations of model calibration
 library(rms)
 
-# Create calibration plots and compute correspoding slopes and intercepts for all models
+# Create calibration plots and compute corresponding slopes and intercepts for all models
+# MELD_1 survival function
 val.prob(stph.meld$MELD.surv, stph.meld$D90_surv, pl = TRUE, smooth = FALSE, logistic.cal = TRUE,
+         xlab = "Predicted survival probability", ylab = "Actual survival probability",
          legendloc = FALSE, statloc = FALSE)
-title("Calibration plot for the MELD1 score")
 
+# MELD_2 survival function
 val.prob(stph.meld$MELD.surv2, stph.meld$D90_surv, pl = TRUE, smooth = FALSE, logistic.cal = TRUE,
+         xlab = "Predicted survival probability", ylab = "Actual survival probability",
          legendloc = FALSE, statloc = FALSE)
-title("Calibration plot for the MELD2 score")
 
+# MELD 3.0
 val.prob(stph.meld$MELD3.surv, stph.meld$D90_surv, pl = TRUE, smooth = FALSE, logistic.cal = TRUE,
+         xlab = "Predicted survival probability", ylab = "Actual survival probability",
          legendloc = FALSE, statloc = FALSE)
-title("Calibration plot for the MELD3.0 score")
 
+# Lille
 val.prob(stph.lille$Lille.surv, stph.lille$D90_surv, pl = TRUE, smooth = FALSE, logistic.cal = TRUE,
+         xlab = "Predicted survival probability", ylab = "Actual survival probability",
          legendloc = FALSE, statloc = FALSE)
-title("Calibration plot for the Lille score")
 
+# CLIF-C ACLF
 val.prob(stph.clif$CLIF.surv, stph.clif$D90_surv, pl = TRUE, smooth = FALSE, logistic.cal = TRUE,
+         xlab = "Predicted survival probability", ylab = "Actual survival probability",
          legendloc = FALSE, statloc = FALSE)
-title("Calibration plot for the CLIF-C ACLF score")
 
-# Calculate the Hosmer-Lemeshow goodness-of-fit statistic for all three models
+# Calculate the Hosmer-Lemeshow goodness-of-fit statistic for all models
 library(ResourceSelection)
 
 hoslem.test(stph.meld$D90_surv, stph.meld$MELD.surv)
