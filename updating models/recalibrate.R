21755f0b54ca587c2a87da1086c24f9be947ff86
solon <karapanagiotissolon@gmail.com>
2022-11-08 15:55:59 +0000

    recalibration code

--- updating models/recalibrate.R
+++ updating models/recalibrate.R
@@ -6,98 +6,86 @@ library(pROC)
 source("val_prob_confidence.R")
 
 # Set random seed
-set.seed(34)
+set.seed(999)
 
 ######
 # Split complete-case sample in training and test observations
-dt <- sort(sample(nrow(stph.c), nrow(stph.c)*.8))
-test.data.c <- stph.c[dt,]
-train.data.c <- stph.c[-dt,]
+fraction <- 0.8
+dt <- sort(sample(nrow(stph.c), nrow(stph.c)*fraction))
+test.data <- stph.c[dt,]
+train.data <- stph.c[-dt,]
 
+#############################################
+############### MELD score ##################
+#############################################
 # Fit a logistic regression model on the training data
-meld_regr <- glm(D90_surv ~ MELD.calc, family = binomial, data = train.data.c)
+meld_regr <- glm(D90_surv ~ MELD.calc, family = binomial("logit"), data = train.data)
 
 # Save the regression coefficients (alpha and beta)
 ic_meld_c <- meld_regr$coefficients[1]
 slope_meld_c <- meld_regr$coefficients[2]
 
 # Calculate adjusted MELD score and survival probability on training set
-train.data.c$updated.meld <- ic_meld_c + slope_meld_c*train.data.c$MELD.calc
-train.data.c$meld.surv.updated <- 1/(1+exp(-train.data.c$updated.meld))
+train.data$updated.meld <- ic_meld_c + slope_meld_c*train.data$MELD.calc
+train.data$meld.surv.updated <- 1/(1 + exp(-train.data$updated.meld))
 
 # Calculate adjusted scores on the test set and assess performance
-test.data.c$updated.meld <- ic_meld_c + slope_meld_c*test.data.c$MELD.calc
-test.data.c$meld.surv.updated <- 1/(1+exp(-test.data.c$updated.meld))
+test.data$updated.meld <- ic_meld_c + slope_meld_c*test.data$MELD.calc
+test.data$meld.surv.updated <- 1/(1 + exp(-test.data$updated.meld))
 
-roc_meld_uc <- roc(test.data.c$D90_surv, test.data.c$meld.surv.updated)
-ci.auc(roc_meld_uc)
-
-val.prob(test.data.c$meld.surv.updated, test.data.c$D90_surv, pl = TRUE, smooth = FALSE, logistic.cal = TRUE,
-         xlab = "Predicted survival probability", ylab = "Actual survival probability",
-         legendloc = FALSE, statloc = FALSE)
-
-# Lille score re-calibration
+#############################################
+################### Lille score #############
+#############################################
 # Run logistic regression and save regression coefficientss
-lille_regr <- glm(D90_surv ~ LILLE, family = binomial, data = train.data.c)
+lille_regr <- glm(D90_surv ~ LILLE, family = binomial, data = train.data)
 
 ic_lille_c <- lille_regr$coefficients[1]
 slope_lille_c <- lille_regr$coefficients[2]
 
 # Update scores in training set
-train.data.c$updated.lille <- ic_lille_c + slope_lille_c*train.data.c$LILLE
-train.data.c$lille.surv.updated <- 1/(1 + exp(-train.data.c$updated.lille))
+train.data$updated.lille <- ic_lille_c + slope_lille_c*train.data$LILLE
+train.data$lille.surv.updated <- 1/(1 + exp(-train.data$updated.lille))
 
 # Assess performance on the test set
-test.data.c$updated.lille <- ic_lille_c + slope_lille_c*test.data.c$LILLE
-test.data.c$lille.surv.updated <- 1/(1 + exp(-test.data.c$updated.lille))
+test.data$updated.lille <- ic_lille_c + slope_lille_c*test.data$LILLE
+test.data$lille.surv.updated <- 1/(1 + exp(-test.data$updated.lille))
 
-roc_lille_uc <- roc(test.data.c$D90_surv, test.data.c$lille.surv.updated)
-ci.auc(roc_lille_uc)
-
-val.prob(test.data.c$lille.surv.updated, test.data.c$D90_surv, pl = TRUE, smooth = FALSE, logistic.cal = TRUE,
-         xlab = "Predicted survival probability", ylab = "Actual survival probability",
-         legendloc = FALSE, statloc = FALSE)
-
-# CLIF-C ACLF score re-calibration
-# Run logistic regression and save regression coefficientss
-clif_regr <- glm(D90_surv ~ CLIF.C, family = binomial, data = train.data.c)
+#############################################
+######### CLIF-C ACLF Score #################
+#############################################
+# Run logistic regression and save regression coefficients
+clif_regr <- glm(D90_surv ~ CLIF.C, family = binomial, data = train.data)
 
 ic_clif_c <- clif_regr$coefficients[1]
 slope_clif_c <- clif_regr$coefficients[2]
 
 # Update scores in training set
-train.data.c$updated.clif <- ic_clif_c + slope_clif_c*train.data.c$CLIF.C
-train.data.c$clif.surv.updated <- 1/(1 + exp(-train.data.c$updated.clif))
+train.data$updated.clif <- ic_clif_c + slope_clif_c*train.data$CLIF.C
+train.data$clif.surv.updated <- 1/(1 + exp(-train.data$updated.clif))
 
 # Assess performance on the test set
-test.data.c$updated.clif <- ic_clif_c + slope_clif_c*test.data.c$CLIF.C
-test.data.c$clif.surv.updated <- 1/(1 + exp(-test.data.c$updated.clif))
+test.data$updated.clif <- ic_clif_c + slope_clif_c*test.data$CLIF.C
+test.data$clif.surv.updated <- 1/(1 + exp(-test.data$updated.clif))
 
-roc_clif_uc <- roc(test.data.c$D90_surv, test.data.c$clif.surv.updated)
-ci.auc(roc_clif_uc)
-
-val.prob(test.data.c$clif.surv.updated, test.data.c$D90_surv, pl = TRUE, smooth = FALSE, logistic.cal = TRUE,
-         xlab = "Predicted survival probability", ylab = "Actual survival probability", 
-         legendloc = FALSE, statloc = FALSE)
-
+###
 # Confidence intervals/SEs for the slope and intercept
 # val_prob_confidence is used to get SEs for the calibration intercepts and slopes (and CIs in the plots)
-val_prob_confidence(test.data.c$meld.surv.updated, test.data.c$D90_surv, pl = TRUE, smooth = FALSE, logistic.cal = TRUE,
+val_prob_confidence(test.data$meld.surv.updated, test.data$D90_surv, pl = TRUE, smooth = FALSE, logistic.cal = TRUE,
                     xlab = "Predicted survival probability", ylab = "Actual survival probability",
                     legendloc = T, roundstats = 3)
 
-val_prob_confidence(test.data.c$clif.surv.updated, test.data.c$D90_surv, pl = TRUE, smooth = FALSE, logistic.cal = TRUE,
+val_prob_confidence(test.data$clif.surv.updated, test.data$D90_surv, pl = TRUE, smooth = FALSE, logistic.cal = TRUE,
                     xlab = "Predicted survival probability", ylab = "Actual survival probability",
                     legendloc = T, dostats = TRUE, roundstats = 3)
 
-val_prob_confidence(test.data.c$lille.surv.updated, test.data.c$D90_surv, pl = TRUE, smooth = FALSE, logistic.cal = TRUE,
+val_prob_confidence(test.data$lille.surv.updated, test.data$D90_surv, pl = TRUE, smooth = FALSE, logistic.cal = TRUE,
                     xlab = "Predicted survival probability", ylab = "Actual survival probability",
                     legendloc = T, dostats = TRUE)
 
 ######
 # Sensitivity analysis; split data on full stph sample and then select the complete cases per model
 # Split full sample in training and test observations
-dt <- sort(sample(nrow(stph), nrow(stph)*.8))
+dt <- sort(sample(nrow(stph), nrow(stph) * fraction))
 test.data <- stph[dt,]
 train.data <- stph[-dt,]
 
