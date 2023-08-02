153bda7bdac031067fcea43e286b07e2e2b02efe
solon <karapanagiotissolon@gmail.com>
2023-08-02 13:38:56 +0100

    imputation sensitivity

---
+++ original models/Calibration_imp_sens.R
@@ -0,0 +1,126 @@
+
+# This script performs all calculations of model calibration
+library(ggplot2)
+library(dplyr)
+library(survminer) # for plotting theme
+
+# funs
+path_funs <- "/Users/work/IDrive-Sync/Projects/MIMAH/code/funs"
+source(paste0(path_funs, "/calibration_fun.R"))
+
+# imputed data
+#path_data <- "/Users/work/IDrive-Sync/Projects/MIMAH/code/AH_code/AH_code/original models/"
+#load(paste0(path_data, "original_models.Rdata"))
+
+#############################################   
+############### Calculate calibration  ######
+#############################################   
+imp_index <- group_split(imp_data3, .imp) 
+
+imp_ind <- max(imp_data3$.imp)
+
+# MELD_1
+cal_MELD.surv1 <- tibble()
+for(i in 1:imp_ind){
+    # MELD 1
+    temp <- calibration(imp_index[[i]]$MELD.surv1, y = imp_index[[i]]$D90_surv)
+    temp$Score <- "MELD_1"
+    temp$delta <- rep(unique(imp_index[[i]]$delta), each = 1068)
+    temp$.imp <- i
+    cal_MELD.surv1 <- rbind(cal_MELD.surv1, temp)
+}
+rm(temp)
+
+# MELD 2
+cal_MELD.surv2 <- tibble()
+for(i in 1:imp_ind){
+    temp <- calibration(imp_index[[i]]$MELD.surv2, y = imp_index[[i]]$D90_surv)
+    temp$Score <- "MELD_2"
+    temp$delta <- rep(unique(imp_index[[i]]$delta), each = 1068)
+    temp$.imp <- i
+    cal_MELD.surv2 <- rbind(cal_MELD.surv2, temp)
+}
+
+rm(temp)
+
+# MELD VanDerwerken
+cal_MELD.VanDerwerken <- tibble()
+for(i in 1:imp_ind){
+    temp <- calibration(imp_index[[i]]$MELD_Van, y = imp_index[[i]]$D90_surv)
+    temp$Score <- "MELD VanDerwerken"
+    temp$delta <- rep(unique(imp_index[[i]]$delta), each = 1068)
+    temp$.imp <- i
+    cal_MELD.VanDerwerken <- rbind(cal_MELD.VanDerwerken, temp)
+}
+
+rm(temp)
+
+# Lille
+cal_Lille <- tibble()
+for(i in 1:imp_ind){
+    for(g in 1:4){
+        d <- unique(imp_index[[i]]$delta)
+        df <- imp_index[[i]] %>% filter(delta == d[g])
+        
+        temp <- calibration(df$Lille.surv, y = df$D90_surv)
+        temp$Score <- "Lille"
+        temp$delta <- unique(df$delta)
+        temp$.imp <- i
+        cal_Lille <- rbind(cal_Lille, temp)
+    }
+}
+
+unique(cal_Lille$delta)
+i <- 1
+imp_index[[i]] %>% group_by(delta) %>% summarise(. ~ calibration, pred = Lille.surv, obs = D90_surv)
+
+
+rm(temp)
+
+# "CLIF-C ACLF" 
+cal_CLIF <- tibble()
+for(i in 1:imp_ind){
+    temp <- calibration(imp_index[[i]]$CLIF.surv, y = imp_index[[i]]$D90_surv)
+    temp$Score <- "CLIF-C ACLF"
+    temp$delta <- rep(unique(imp_index[[i]]$delta), each = 1068)
+    temp$.imp <- i
+    cal_CLIF <- rbind(cal_CLIF, temp)
+}
+
+# combine dfs
+df_cal <- rbind(cal_MELD.surv1, cal_MELD.surv2, cal_MELD.VanDerwerken, cal_Lille, cal_CLIF)
+
+#############################################   
+###################### Plots  ###############
+############################################# 
+
+# plot without ribbon and without MELD 3.0
+cal_Lille %>% filter(Score == "Lille") %>%
+    ggplot(., aes(x = pred, y = obs, col = delta)) +
+    geom_line(aes(group = .imp))  + 
+    geom_abline(slope = 1, intercept = 0, linetype = "dashed") +
+    #geom_ribbon(aes(ymin = lower, ymax = upper, linetype = NA), 
+    #            alpha = 0.3, show.legend = F) + 
+    #scale_fill_manual("", values = col) + 
+    #scale_color_manual(name = "Score", values = col) + 
+    facet_grid(delta ~ .) +
+    xlim(0, 1) + 
+    ylim(0, 1) + 
+    ylab("Observed proportion") + 
+    xlab("Predicted probability") + 
+    theme_classic() +
+    theme(legend.position = "none")
+
+
+cal_Lille %>% ggplot(.) +
+    geom_boxplot(aes(x = pred, fill = delta))
+
+imp_data3 %>% select(Lille.surv, delta, .imp) %>%
+    group_by(delta) %>%
+    ggplot(.) +
+    geom_boxplot(aes(Lille.surv, fill = delta)) 
+
+
+imp_data3 %>% select(Lille.surv, delta, .imp) %>%
+    group_by(delta) %>% summarise(mean(Lille.surv))
+
--- pre-analysis/imputation.R
+++ pre-analysis/imputation.R
@@ -1,13 +1,13 @@
 
-
 library(mice)
 library(dplyr)
+library(purrr)
 
 # data
 path_data <- "/Users/work/IDrive-Sync/Projects/MIMAH/code/AH_code/AH_code/pre-analysis/"
 load(paste0(path_data, "full_sample.Rdata"))
 
-
+# vars to use in the imputation
 vars_MELD <- c("Creatinine.mg.dl", "Bilirubin.mg.dl", "INR", "Sodium")
 vars_MELD3 <- c(vars_MELD, "Albumin", "Gender")
 vars_Lille <- c("Age", "Albumin", "Bilirubin.day.7", "Bilirubin.mg.dl", "protime")
@@ -17,40 +17,70 @@ stph_short <- stph %>%
     rename(Age = Age.at.randomisation..calc.) %>%
     select(D90_DTH, all_of(vars_MELD), all_of(vars_MELD3), all_of(vars_Lille), all_of(vars_CLIF))
 
-# relationship between Bilirubin value
-#plot(stph$Bilirubin.mg.dl*17, stph$Bilirubin.Merged)
-#abline(a = 0, b = 1)
 
+# inspect the missing data pattern
+md.pattern(stph_short, rotate.names = T)
 
-
-dim(stph_short)
-
-summary(stph_short)
-
-
-
-
-md.pattern(stph_short)
-
-
+# multiple imputation
 imp <- mice(stph_short, m = 20, method = "pmm", seed = 5678)
 
-plot(imp)
-
+# predictor matrix
+# https://www.gerkovink.com/miceVignettes/Convergence_pooling/Convergence_and_pooling.html 
 imp$pred
 
+# inspect the convergence of the algorithm
+plot(imp)
 
+# imputation method
 imp$method #pmm method
 
+# diagnostic checking
+stripplot(imp, Bilirubin.day.7 ~ .imp, pch = 20, cex = 2)
+stripplot(imp)
 
-stripplot(imp, Bilirubin.day.7 ~ .imp, pch=20, cex=2)
+summary(stph_short$Bilirubin.day.7)
+summary(with(imp, mean(Bilirubin.day.7)))
+
+densityplot(imp, ~ Bilirubin.day.7)
 
 imp$where 
 
+# collection of the m imputed data sets 
 imp_data <- complete(imp, action = "long")
 
 imp_data[which(is.na(imp_data)),]
 
+###########################################################
+################### Sensitivity analysis ##################
+###########################################################
+# create  vector that represent the following adjustment values for Bili day 7: 0 for MAR, and -50, -15, and -20 for MNAR.
+delta <- c(0, -50, -100, -200)
 
-imp_data %>% filter(.id == 3)
+# perform a dry run (using maxit = 0)
+ini <- mice(stph_short, maxit = 0)
+
+imp.all <- vector("list", length(delta))
+post <- ini$post
+for (i in 1:length(delta)){
+    d <- delta[i]
+    cmd <- paste("imp[[j]][,i] <- squeeze(imp[[j]][,i] +", d, ",c(0, 1061))")
+    post["Bilirubin.day.7"] <- cmd
+    imp <- mice(stph_short, post = post, m = 20, maxit = 30, seed = i, print = FALSE)
+    imp.all[[i]] <- imp
+}
+
+bwplot(imp.all[[1]])
+bwplot(imp.all[[4]])
+
+densityplot(imp.all[[1]], lwd = 3)
+densityplot(imp.all[[4]], lwd = 3)
+
+summary(with(imp.all[[3]], range(Bilirubin.day.7)))
+
+## post-process
+names(imp.all) <- delta # add names to list
+complete_list <- lapply(imp.all, complete, action = "long")
+imp_sens_df <- map_df(complete_list, ~ as.data.frame(.x), .id = "delta")
+
+
 
